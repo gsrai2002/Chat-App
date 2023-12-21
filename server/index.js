4c4d2457b37f6e134687ed6f1326542f6d8e9e7c
Ghanshyam Rai <gsrai082002@gmail.com>
2023-12-21 12:04:53 +0530

    Base setup is complete

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const app = express();
+require("dotenv").config();
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongoose.connect(DB).then(() =>console.log('DB connection successful'));
+
+app.use(cors());
+app.use(express.json());
+
+const server = app.listen(process.env.PORT, ()=>{
+    console.log(`Server started on Port ${process.env.PORT}`);
+});
