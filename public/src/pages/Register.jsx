e2f93af1253fceb60b76de33c739c188751350b5
Ghanshyam Rai <gsrai082002@gmail.com>
2023-12-23 11:01:40 +0530

    Register functionality is complete

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -1,15 +1,67 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, {useState,useEffect} from 'react';
+import axios from "axios";
+import {Link, useNavigate} from 'react-router-dom';
 import styled from 'styled-components';
 import Logo from '../assets/logo.svg';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { registerRoute } from "../utils/APIRoutes";
 
 function Register() {
-    const handleSubmit = (event) =>{
-        event.preventDefault();
-        alert("form");
-    }
-    const handleChange = (event) =>{
+    const navigate = useNavigate();
+    const [values, setValues] = useState({
+        username: "",
+        email: "",
+        password: "",
+        confirmPassword: ""
+    });
 
+    const toastOptions = {
+        position: "bottom-right",
+        autoClose: 8000,
+        pauseOnHover: true,
+        draggable: true,
+        theme: "dark",
+    };
+
+    const handleSubmit = async (event) =>{
+        event.preventDefault();
+        if(handleValidation()){
+            console.log("in validation", registerRoute);
+            const { email, username, password } = values;
+            const {data} = await axios.post(registerRoute, {username,email,password});
+
+            if(data.status === false){
+                toast.error(data.msg, toastOptions); 
+            }
+            if(data.status === true){
+                localStorage.setItem('chat-app-user',JSON.stringify(data.user));
+                navigate("/");
+            }
+        };  
+    }
+
+    const handleValidation = () => {
+        const {password, confirmPassword, username, email} = values;
+        if(password !== confirmPassword){
+            toast.error("password and confirm password should be same", toastOptions);
+            return false;
+        } else if(username.length < 3){
+            toast.error("Username should be greater than 3 characters", toastOptions);
+            return false;
+        } else if(password.length < 8){
+            toast.error("Password should be equal or greater than 8 characters", toastOptions);
+            return false;
+        } else if(email === ""){
+            toast.error("Email is required.", toastOptions);
+            return false;
+        }
+
+        return true;
+    };
+
+    const handleChange = (event) =>{
+        setValues({...values, [event.target.name]: event.target.value});
     }
   return (
     <>
@@ -29,6 +81,7 @@ function Register() {
                 </span>
             </form>
         </FormContaier>
+        <ToastContainer />
     </>
   )
 }
--- server/index.js
+++ server/index.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 require("dotenv").config();
 
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
-
 mongoose.connect(DB).then(() =>console.log('DB connection successful'));
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth",userRoutes);
 
 const server = app.listen(process.env.PORT, ()=>{
     console.log(`Server started on Port ${process.env.PORT}`);
